85e20246370b314330e0a41842bbfd306201218e
betashepherd <betashepherd@gmail.com>
2023-05-17 14:56:45 +0800

    1. send data 2. diplay on oled

--- app/fs/init.js
+++ app/fs/init.js
@@ -1,9 +1,51 @@
+load('api_arduino_ssd1306.js');
 load('api_config.js');
 load('api_events.js');
 load('api_gpio.js');
 load('api_mqtt.js');
 load('api_timer.js');
 load('api_sys.js');
+load('api_uart.js');
+
+
+
+
+
+//////////////////////////
+// e32 module init
+//////////////////////////
+let e32_m0 = 5; // D1
+let e32_m1 = 4; // D2
+/////////////////////////////////
+GPIO.set_mode(e32_m0, GPIO.MODE_OUTPUT);
+GPIO.set_mode(e32_m1, GPIO.MODE_OUTPUT);
+
+//set work mode
+GPIO.write(e32_m0, 0);
+GPIO.write(e32_m1, 0);
+
+Timer.set(1500, 0, function () {
+  // Wait for sometime for e32 module init work mode
+}, null);
+///////////////////////////////
+// e32 module init end
+///////////////////////////////
+
+//////////////////////////
+// Initialize Adafruit_SSD1306 library (I2C)
+let d = Adafruit_SSD1306.create_i2c(14, Adafruit_SSD1306.RES_128_64);
+// Initialize the display.
+d.begin(Adafruit_SSD1306.SWITCHCAPVCC, 0x3C, true /* reset */);
+d.display();
+let showStr = function(d, str) {
+  d.clearDisplay();
+  d.setTextSize(2);
+  d.setTextColor(Adafruit_SSD1306.WHITE);
+  d.setCursor(d.width() / 4, d.height() / 4);
+  d.write(str);
+  d.display();
+};
+//////////////////////////
 
 let btn = Cfg.get('board.btn1.pin');              // Built-in button GPIO
 let led = Cfg.get('board.led1.pin');              // Built-in LED GPIO number
@@ -49,3 +91,42 @@ if (btn >= 0) {
     }
   }, null);
 }
+
+let uartNo = 0;   // Uart number used for this example
+let rxAcc = '';   // Accumulated Rx data, will be echoed back to Tx
+// Configure UART at 115200 baud
+UART.setConfig(uartNo, {
+  baudRate: 115200,
+});
+
+// // Set dispatcher callback, it will be called whenver new Rx data or space in
+// // the Tx buffer becomes available
+// UART.setDispatcher(uartNo, function(uartNo) {
+//   let ra = UART.readAvail(uartNo);
+//   if (ra > 0) {
+//     // Received new data: print it immediately to the console, and also
+//     // accumulate in the "rxAcc" variable which will be echoed back to UART later
+//     let data = UART.read(uartNo);
+//     print('Received UART data:', data);
+//     rxAcc += data;
+//   }
+// }, null);
+//
+// // Enable Rx
+// UART.setRxEnabled(uartNo, true);
+
+// Send UART data every second
+Timer.set(1000 /* milliseconds */, Timer.REPEAT, function() {
+  UART.write(uartNo, 'AF01');
+  UART.write(uartNo, '17');
+  let data = 'Hello E32! ' + '\n';
+  UART.write(uartNo, data);
+  showStr(d, data);
+  rxAcc = '';
+}, null);
+
+// Timer.set(1000 /* milliseconds */, Timer.REPEAT, function() {
+//   showStr(d, "i = " + JSON.stringify(i));
+//   print("i = ", i);
+//   i++;
+// }, null);
